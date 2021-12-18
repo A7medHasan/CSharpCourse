5a633ea022843a566a4177c9926559b13ad35dda
A7medHasan <96312965+A7medHasan@users.noreply.github.com>
2021-12-18 10:41:04 -0800

    restructure

--- Fundamental/Program.cs
+++ Fundamental/Program.cs
@@ -1,7 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-// variables //
-int x  
-    string b 
+// variables // just like variables in math (algebra) where variables like x store a value,
+//              variables in programming also stores a value and  but the differece is that
+//              in math the value is hidden while in programming you set the value and can
+//              change it in run time.
 
+int x = 5; // variables can store numbers
+string b = "Apple"; // or store a text
+bool c = true; // or special values like true or false 
+
+// to know the difference and when to use a data type, you need to memorise this table.
+// Data Type  |	Size   |  Description
+// ---------------------------------------
+// int         4 bytes   Stores whole numbers from -2,147,483,648 to 2,147,483,647
+// long	       8 bytes	 Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// float	   4 bytes	 Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+// double	   8 bytes	 Stores fractional numbers. Sufficient for storing 15 decimal digits
+// bool	       1 bit	 Stores true or false values
+// char	       2 bytes	 Stores a single character/letter, surrounded by single quotes
+// string	   2 bytes   Stores a sequence of characters, surrounded by double quotes
+//             per character	
+
+
+Int32 x = 5;
+int y = 10;
